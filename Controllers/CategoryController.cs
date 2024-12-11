4d284a6d71cd81e90b66d20e6dad3413e35474f7
J0HANNY <135481045+J0HANNY@users.noreply.github.com>
2024-12-11 04:52:43 -0400

    Web

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.DAL.Interfaces;
+using Shop.DAL.Models;
+
+namespace Shop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryDb _daoCategory;
+
+        public CategoryController(ICategoryDb daoCategory)
+        {
+            _daoCategory = daoCategory;
+        }
+
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            var category = _daoCategory.GetCategories();
+            return View(category);
+        }
+
+        // GET: CategoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            var category = _daoCategory.GetCategoryId(id);
+            return View(category);
+        }
+
+        // GET: CategoryController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoryController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CategoryCreateUpdateModel createModel)
+        {
+            try
+            {
+                createModel.ModifyUser = 1;
+                createModel.ModifyDate = DateTime.Now;
+                _daoCategory.CreateCategory(createModel);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoryController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var category = _daoCategory.GetCategoryId(id);
+            return View(category);
+        }
+
+        // POST: CategoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CategoryCreateUpdateModel updateModel)
+        {
+            try
+            {
+                updateModel.ModifyUser = 1;
+                updateModel.ModifyDate = DateTime.Now;
+                _daoCategory.ModifyCategory(updateModel);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Shop.DAL.Context;
+using Shop.DAL.Daos;
+using Shop.DAL.Interfaces;
+
+namespace Shop.Web
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<Shop.DAL.Context.ShopContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ShopDb")));
+
+            builder.Services.AddScoped<ICategoryDb, DaoCategory>();
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+
+        }
+    }
+}
